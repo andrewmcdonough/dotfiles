4b68f09975d6b0e256d20bcd5b04f21fd6e58ad3
Andrew McDonough <andrew@andrewmcdonough.co.uk>
2019-01-31 20:50:29 +0000

    Add install script

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Andrew McDonough's Configuration Files
 
 I'm in the process of cleaning up my dotfiles, and decided to start with a clean repository. I'm slowly cleaning up years of config bloat, and copying configurations across one at a time.
+
+## Installation
+
+Clone the repo into ~/dotfiles, then run the install script. This symlinks the config files in your home directory.
+
+```
+sh install.sh
+```
---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+ln -s ~/dotfiles/vimrc ~/.vimrc
+ln -s ~/dotfiles/gitconfig ~/.gitconfig
+
+#ln -s ~/dotfiles/ackrc ~/.ackrc
+#ln -s ~/dotfiles/bashrc ~/.bashrc
+#ln -s ~/dotfiles/git-completion ~/.git-completion
+#ln -s ~/dotfiles/gitignore_global ~/.gitignore_global
+#ln -s ~/dotfiles/irbrc ~/.irbrc
+#ln -s ~/dotfiles/lynxrc ~/.lynxrc
+#ln -s ~/dotfiles/pryrc ~/.pryrc
+#ln -s ~/dotfiles/rspec ~/.rspec
+#ln -s ~/dotfiles/shell_aliases ~/.shell_aliases
+#ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+#ln -s ~/dotfiles/vimrc ~/.vimrc
+#ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+#ln -s ~/dotfiles/zshrc ~/.zshrc
+#ln -s ~/dotfiles/eshintrc ~/.eshintrc
