d0279bf084ec9800660f49c747e7bdfb857cda47
Andrew McDonough <andrew@andrewmcdonough.co.uk>
2021-09-02 13:02:33 +0200

    Install chruby on setup

--- install.sh
+++ install.sh
@@ -28,3 +28,9 @@ ln -s -f ~/dotfiles/init.vim ~/.config/nvim/init.vim
 # Install vimplug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
          https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# Install chruby
+wget -O chruby-0.3.9.tar.gz https://github.com/postmodern/chruby/archive/v0.3.9.tar.gz
+tar -xzvf chruby-0.3.9.tar.gz
+cd chruby-0.3.9/
+sudo make install
