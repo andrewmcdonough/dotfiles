b44737c53d598894d0db4a88bebd6a11d0c7e55b
Andrew McDonough <andrew@andrewmcdonough.co.uk>
2022-12-01 11:49:33 +0100

    Add shortcuts for a smaller screen

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -3,6 +3,11 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
   hs.reload()
 end)
 
+-- Keyboard shortcuts
+
+columns = 9
+
+-- Send window to left column (widescreen)
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "h", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
@@ -16,6 +21,7 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "h", function()
   win:setFrame(f)
 end)
 
+-- Send window to right column (widescreen)
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "l", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
@@ -29,6 +35,7 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "l", function()
   win:setFrame(f)
 end)
 
+-- Send window to middle (widescreen)
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "k", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
@@ -40,4 +47,83 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "k", function()
   f.y = 0
   f.h = max.h
   win:setFrame(f)
+end)
+
+-- Send window to left of middle column (widescreen)
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, ",", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.w * 2 / 9
+  f.w = max.w * 5 / 18
+  f.y = 0
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- Send window to right of middle column (widescreen)
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, ".", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.w * 1 / 2
+  f.w = max.w * 5 / 18
+  f.y = 0
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+
+-- Send window to left 50%
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Left", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = 0 
+  f.w = max.w * 1 / 2
+  f.y = 0
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+
+-- Send window to right 50%
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Right", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.w * 1 / 2
+  f.w = max.w * 1 / 2
+  f.y = 0
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- Send window to fullscreen
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Up", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = 0
+  f.w = max.w
+  f.y = 0
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- Minimize window
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Down", function()
+  local win = hs.window.focusedWindow()
+
+  win:minimize()
 end)
